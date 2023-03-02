8faa98122d951adefbdb21ed5c6a0e721247aca8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-03-01 22:55:16 -0800

    Recursive Option + Hidden Hidden Files
    Added a check to hide hidden files from the list output, which will make it a bit easier to find the right kinds of files I'm looking for, specifically the library's song files.
    
    Added options to disable the previously listed addition, and the existing recursive behavior of getting the files within child directories. I also added some JSDoc documentation to describe the function a bit, too.
    
    https://stackoverflow.com/questions/50121881/node-js-recursively-list-full-path-of-files

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,22 @@ for (const file of files){
   console.log(file);
 }
 
-export async function * getFiles(directory: string): AsyncGenerator<string,void,unknown> {
+export interface GetFilesOptions {
+  recursive?: boolean;
+  includeHidden?: boolean;
+}
+
+/**
+ * Creates an AsyncGenerator object which returns the paths for all files within a directory.
+*/
+export async function * getFiles(directory: string, { recursive = true, includeHidden = false }: GetFilesOptions = {}): AsyncGenerator<string,void,unknown> {
   for (const entry of await readdir(directory,{ withFileTypes: true })){
     const path = resolve(directory,entry.name);
 
-    if (entry.isDirectory()){
+    if (entry.isDirectory() && recursive){
       yield * getFiles(path);
     } else {
+      if (entry.name.startsWith(".") && !includeHidden) continue;
       yield path;
     }
   }
